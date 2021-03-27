5242c6e158eee586816c99146fbd5515f8c64d08
breynergarzon <breynersgarzon@gmail.com>
2021-03-27 13:30:51 -0500

    incorporación DataAnnotations

--- Carvajal.Prueba.Model/View/UserView.cs
+++ Carvajal.Prueba.Model/View/UserView.cs
@@ -1,18 +1,19 @@
-// using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace Carvajal.Prueba.Model.View
 {
     public class UserView
     {
-        
+
         public string Name { get; set; }
         public string LastName { get; set; }
         public int DocumentType { get; set; }
         public string DocumentNumber { get; set; }
-        // [Required]
-        // [Range(0D, (double)1, ErrorMessage = "La contraseñadebe de poseer una mínima cantidad de 8 carácteres.")]
+        [Required]
+        [StringLength(12, MinimumLength = 5, ErrorMessage="El tamaño mínimo de la contraseña es de 5 carácteres y de máximo 12.")]
         public string Password { get; set; }
-
+        [Required]
+        [EmailAddress(ErrorMessage = "El mail suministrado es inválido por favor intente nuevamente.")]
         public string Mail { get; set; }
     }
 }
